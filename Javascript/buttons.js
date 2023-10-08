b376bdfdb712211073c0b1b0ac3ec57a03d59b24
mattiamanna2203 <mattiamanna2203@gmail.com>
2023-10-08 14:56:06 +0200

    Importazione Javascript tramite link

---
+++ Javascript/buttons.js
@@ -0,0 +1,12 @@
+  
+/*
+    <script type="text/javascript" src="./JavaScript/buttons.js"></script> <!--In questo script sono contenuti gli event listener Java.-->
+
+   <!--Importare JavaScript-->
+      <script type="text/javascript" src="./JavaScript/create_table.js"></script> <!--JavaScript: Input JSON, Output Tabella ben formattata-->
+
+
+    <!--Importare JavaScript-->
+      <script type="text/javaScript" src="https://raw.githubusercontent.com/mattiamanna2203/cameracafe_searchengine/master/Javascript/create_table.js"></script><!--JavaScript: Input JSON, Output Tabella ben formattata-->
+
+*/
