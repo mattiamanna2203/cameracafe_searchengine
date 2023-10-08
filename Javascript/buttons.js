d0f38a198171059ba3a1dd2b023591c6167c393a
mattiamanna2203 <mattiamanna2203@gmail.com>
2023-10-08 14:23:28 +0200

    Implementata la funzione onkeydown

--- Javascript/buttons.js
+++
@@ -1,9 +0,0 @@
-const button= document.querySelector(".avvio");
-const stringa= document.querySelector(".barra-ricerca");
-button.addEventListener('click',() =>{
-                                       console.log('ciao');   
-                                       console.log(stringa.value);}) 
-
-
-
-// Construct the URL to the raw content of the file
--- PyScript/events_listeners.py
+++ PyScript/events_listeners.py
@@ -11,15 +11,50 @@ from pyodide.ffi.wrappers import add_event_listener
 #%% Funzione che avvia la ricerca.
 def avvio_ricerca(*arg):
    """
-      Funzione che associa all'event listener per al tasto di avvio di ricerca("#avvio-ricerca") la pipeline python da seguire.
-      Lavorando con PyScript questo è l'unico modo che  per ora ho trovato per utilizzare correttamente degli eventlistener.
-   """
-  stringa= document.querySelector(".barra-ricerca") #Estrarre la stringa digitata dall'utente.
-  output = query_ranking(stringa.value,100) #Richiamare la funzione
-  sessionStorage.setItem('database',output.to_json(orient='records')) #Salvare localmente un file.
-  dati = sessionStorage.getItem('database'); # Utilizzare un file salvato localmente.
-  js.import_json('database') #Scrivendo js.nomedellafunzione, si potrà utilizzare    una funzione presente nei file JavaScript importati. 
-                             #Permette quindi di far lavorare linguaggio Python3 e JavaScript insieme.
+   Funzione che associa all'event listener del tasto di avvio di ricerca("#avvio-ricerca") la pipeline python da seguire.
 
-avv = document.getElementById("avvio-ricerca") #Puntare
+   Lavorando con PyScript questo è l'unico modo che  per ora ho trovato per utilizzare correttamente degli eventlistener.
+
+   Questa funzione non necessita argomenti, le informazioni necessarie sono direttamente ricavate dall'html tramite querySelector.
+   """
+   #Estrarre la stringa digitata dall'utente.
+   stringa = document.querySelector(".barra-ricerca")
+
+   #Richiamare la funzione
+   output = query_ranking(stringa.value,100) 
+
+   #Salvare localmente un file.
+   sessionStorage.setItem('database',output.to_json(orient='records')) 
+
+   # Utilizzare un file salvato localmente.
+   dati = sessionStorage.getItem('database'); 
+
+   #Scrivendo js.nomedellafunzione, si potrà utilizzare    una funzione presente nei file JavaScript importati. 
+   #Permette quindi di far lavorare linguaggio Python3 e JavaScript insieme. 
+   js.import_json('database') 
+
+
+#Aggiungere un event listener alla pressione di un tasto
+def handle_keypress(event):
+   """
+   Funzione che associa all'event listener PRESSIONE TASTO ENTER la pipeline python da seguire.
+
+   Questa funzione non necessita argomenti, le informazioni necessarie sono direttamente ricavate dall'html tramite querySelector.
+
+   Questa funzione permette di elaborare il tasto cliccato dall'utente. Così da assegnare al tasto una funzione specifica. 
+
+   Informazioni trovate al link: https://www.cnblogs.com/xgqfrms/p/16513674.html
+   """
+   if event.key =="Enter":
+      avvio_ricerca()
+
+
+
+#Selezionare il tasto di avvio ricerca
+avv = document.getElementById("avvio-ricerca") 
+
+#Aggiungere un event listener al tasto di avvio ricerca, quando si premerà il tasto verrà richiamata la funzione avvio_ricerca(*arg)
 add_event_listener(avv, "click", avvio_ricerca)
+
+#Aggiungere un event listener alla pressione del tasto ENTER. Quando si premerà il tasto verrà richiamata la funzione  avvio_ricerca(*arg)  passando per la funzione handle_keypress che verifica che il tasto premuto sia Enter.
+add_event_listener(document.body,"keypress",handle_keypress)
