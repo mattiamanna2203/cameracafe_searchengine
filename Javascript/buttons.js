de9bfa94befe2adbaf3f03c6027667d51e71feb5
mattiamanna2203 <mattiamanna2203@gmail.com>
2023-10-08 15:01:14 +0200

    Risoluzione bug importazione javascript

--- Javascript/buttons.js
+++ Javascript/buttons.js
@@ -1,5 +1,11 @@
   
 /*
+
+    <script src="JavaScript/buttons.js"></script> <!--In questo script sono contenuti gli event listener Java.-->
+
+   <!--Importare JavaScript-->
+    <script  src="./JavaScript/create_table.js"></script> <!--JavaScript: Input JSON, Output Tabella ben formattata-->
+
     <script type="text/javascript" src="./JavaScript/buttons.js"></script> <!--In questo script sono contenuti gli event listener Java.-->
 
    <!--Importare JavaScript-->
@@ -8,5 +14,9 @@
 
     <!--Importare JavaScript-->
       <script type="text/javaScript" src="https://raw.githubusercontent.com/mattiamanna2203/cameracafe_searchengine/master/Javascript/create_table.js"></script><!--JavaScript: Input JSON, Output Tabella ben formattata-->
+      <!--Importare JavaScript-->
+      <script src="https://raw.githack.com/mattiamanna2203/cameracafe_searchengine/master/Javascript/create_table.js"></script><!--JavaScript: Input JSON, Output Tabella ben formattata-->
+
+      <script src="https://raw.githack.com/mattiamanna2203/cameracafe_searchengine/master/Javascript/buttons.js"></script><!--JavaScript: Input JSON, Output Tabella ben formattata-->
 
 */
